5359da4dcfe625c265d73325d75793dee4b68c39
ohju96 <ohju96@naver.com>
2022-10-15 22:52:43 +0900

    :tada: Project Setting

---
+++ Forgetting-Curve-Project/src/test/java/com/example/forgettingcurveproject/ForgettingCurveProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forgettingcurveproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgettingCurveProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
