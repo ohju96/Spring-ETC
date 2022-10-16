cf72208414b9839ce717f09ed26ff687c283d441
ohju96 <ohju96@naver.com>
2022-10-16 19:40:23 +0900

    :sparkles: exception setting

--- Forgetting-Curve-Project/src/main/java/com/example/forgettingcurveproject/exception/UserException.java
+++ Forgetting-Curve-Project/src/main/java/com/example/forgettingcurveproject/exception/UserException.java
@@ -1,2 +1,11 @@
-package com.example.forgettingcurveproject.exception;public class UserException {
+package com.example.forgettingcurveproject.exception;
+
+import com.example.forgettingcurveproject.exception.result.UserExceptionResult;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class UserException extends RuntimeException{
+    private final UserExceptionResult errorResult;
 }
--- Forgetting-Curve-Project/src/main/java/com/example/forgettingcurveproject/exception/result/UserExceptionResult.java
+++ Forgetting-Curve-Project/src/main/java/com/example/forgettingcurveproject/exception/result/UserExceptionResult.java
@@ -1,2 +1,20 @@
-package com.example.forgettingcurveproject.exception.result;public enum UserExceptionResult {
+package com.example.forgettingcurveproject.exception.result;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserExceptionResult {
+    DUPLICATE_USER_EMAIL(HttpStatus.BAD_REQUEST, "중복된 사용자 이메일입니다."),
+    UNAUTHORIZED_REQUEST(HttpStatus.BAD_REQUEST, "해당 정보에 접근 권한이 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 정보를 찾을 수 없습니다."),
+    UNKNOWN_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "Unknown Exception"),
+    USER_PASSWORD_NOT_MATCHED(HttpStatus.BAD_REQUEST, "비밀번호를 확인해주세요."),
+    EXPIRED_JWT_TOKEN(HttpStatus.FORBIDDEN, "만료된 토큰 입니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
 }
