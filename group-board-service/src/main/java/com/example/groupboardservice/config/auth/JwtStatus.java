071c9afdcfbe368787a61bb4f5e24105cc441678
ohju96 <ohju96@naver.com>
2022-12-24 11:41:44 +0900

    [셋팅] : 브랜치 정리 및 프로젝트 셋팅

--- group-board-service/src/main/java/com/example/groupboardservice/config/auth/JwtStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.groupboardservice.config.auth;
-
-public enum JwtStatus {
-    ACCESS, // 유효한 토큰
-    DENIED, // 유효하지 앟은 토큰
-    EXPIRED // 만료된 토큰
-}
--- group-board-service/src/main/java/com/example/groupboardservice/config/auth/JwtTokenType.java
+++
@@ -1,6 +0,0 @@
-package com.example.groupboardservice.config.auth;
-
-public enum JwtTokenType {
-    ACCESS_TOKEN,
-    REFRESH_TOKEN
-}
