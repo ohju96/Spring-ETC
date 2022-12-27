cf6e64d0d11b350c6a61c6d5fcefaa4b2b37cfb3
ohju96 <ohju96@naver.com>
2022-12-28 02:24:10 +0900

    [기능] : 동시성 문제를 Synchronized를 사용해서 해결하기

---
+++ springDemo/src/main/java/com/example/springdemo/service/StockService.java
@@ -0,0 +1,25 @@
+package com.example.springdemo.service;
+
+import com.example.springdemo.domain.Stock;
+import com.example.springdemo.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class StockService {
+
+    private final StockRepository stockRepository;
+
+    // synchronized 해당 메소드에 1개의 스레드만 접근할 수 있게 한다.
+//    @Transactional기 (Synchronized사용 시 주석 처리 필수)
+    public synchronized void decrease(Long id, Long quantity) {
+
+        Stock stock = stockRepository.findById(id).orElseThrow();
+
+        stock.decrease(quantity);
+
+        stockRepository.saveAndFlush(stock);
+    }
+}
